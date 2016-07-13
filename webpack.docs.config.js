16733f5510e9b6fb0d1d9af37ee1a441696da5f3
simonguo <simonguo.2009@gmail.com>
2016-07-14 00:08:46 +0800

    Update docs

--- webpack.docs.config.js
+++ webpack.docs.config.js
@@ -1,21 +1,33 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const isPublish = process.env.NODE_ENV === 'publish';
-const plugins = [];
+const plugins = [
+   new ExtractTextPlugin('[name].css')
+];
 
 if (isPublish) {
+
     plugins.push(new webpack.optimize.UglifyJsPlugin({
         compress: {
             warnings: false
         }
     }));
+
     plugins.push(new webpack.BannerPlugin(`Last update: ${new Date().toString()}`));
+    //plugins.push(new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'));
+    plugins.push(new webpack.optimize.CommonsChunkPlugin({
+        names: ['data','vendor']
+    }));
+
 }
 
 module.exports = {
     entry: {
-        index: path.join(__dirname, 'docs')
+        index: path.join(__dirname, 'docs'),
+        data:['faker'],
+        vendor: ['react', 'react-dom', 'lodash', 'classnames', 'dom-lib']
     },
     output: {
         path: path.join(__dirname, 'docs/assets'),
@@ -23,24 +35,19 @@ module.exports = {
     },
     plugins: plugins,
     module: {
-        loaders: [{
-            test: /\.js$/,
-            loaders: [
-                'babel?babelrc'
-            ],
-            exclude: /node_modules/
-        }, {
-            test: /\.less$/,
-            loaders: [
-                'style',
-                'css?minimize',
-                'less'
-            ]
-        }],
-        postLoaders: [{
-            test: /\.js$/,
-            loaders: ['es3ify-loader'],
-        }]
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: [
+                    'babel?babelrc'
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test:/\.less$/,
+                loader:  ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
+            },
 
+        ]
     }
 };
