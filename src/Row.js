fd4ede0dbde3bc36b64be003bdf05aecb998147e
simonguo <simonguo.2009@gmail.com>
2017-07-18 19:10:53 +0800

    Update Row*

--- src/Row.js
+++ src/Row.js
@@ -1,32 +1,27 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import ClassNameMixin from './mixins/ClassNameMixin';
-import ReactComponentWithPureRenderMixin from './mixins/ReactComponentWithPureRenderMixin';
+import decorate from './utils/decorate';
 
-import { assign } from 'lodash';
-const Row = React.createClass({
-  mixins: [
-    ClassNameMixin,
-    ReactComponentWithPureRenderMixin
-  ],
-  PropTypes: {
-    width: PropTypes.number,
-    height: PropTypes.number,
-    headerHeight: PropTypes.number,
-    top: PropTypes.number,
-    style: PropTypes.object,
-    isHeaderRow: PropTypes.bool
-  },
-  getDefaultProps() {
-    return {
-      height: 36,
-      headerHeight: 36,
-      isHeaderRow: false
-    };
-  },
+const propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  headerHeight: PropTypes.number,
+  top: PropTypes.number,
+  style: PropTypes.object,
+  isHeaderRow: PropTypes.bool
+};
+
+const defaultProps = {
+  height: 36,
+  headerHeight: 36,
+  isHeaderRow: false
+};
+
+class Row extends React.Component {
   render() {
+
     const {
-      children,
       className,
       width,
       height,
@@ -37,30 +32,30 @@ const Row = React.createClass({
       ...props
     } = this.props;
 
-    let classes = classNames(
-      this.prefix('row'),
-      isHeaderRow ? this.prefix('row-header') : '',
-      className
-    );
+    const classes = classNames(this.prefix('row'), {
+      [this.prefix('row-header')]: isHeaderRow
+    }, className);
 
-    let styles = assign({
+    const styles = {
       minWidth: width,
       height: isHeaderRow ? headerHeight : height,
-      transform: `translate3d(0px, ${top}px, 0px)`
-      //top
-    }, style);
+      transform: `translate3d(0px, ${top}px, 0px)`,
+      ...style
+    };
+
+    const elementProps = _.omit(props, Object.keys(propTypes));
 
     return (
       <div
+        {...elementProps}
         className={classes}
         style={styles}
-        {...props}
-      >
-        {children}
-      </div>
+      />
     );
   }
+}
 
-});
+Row.propTypes = propTypes;
+Row.defaultProps = defaultProps;
 
-export default Row;
+export default decorate()(Row);
