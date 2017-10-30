8ad03ba2a0a25b7016ac5490c60938a31ca34327
simonguo <simonguo.2009@gmail.com>
2017-10-30 13:51:05 +0800

    Added compression-webpack-plugin for production ENV

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlwebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CompressionPlugin = require("compression-webpack-plugin");
 const markdownLoader = require('markdownloader').renderer;
 const { NODE_ENV } = process.env;
 
@@ -34,6 +35,13 @@ const plugins = [
 if (process.env.NODE_ENV === 'production') {
   plugins.push(new webpack.optimize.UglifyJsPlugin());
   plugins.push(new webpack.BannerPlugin({ banner: `Last update: ${new Date().toString()}` }));
+  plugins.push(new CompressionPlugin({
+    asset: "[path].gz[query]",
+    algorithm: "gzip",
+    test: /\.(js|html)$/,
+    threshold: 10240,
+    minRatio: 0.8
+  }));
 }
 const common = {
   entry: path.resolve(__dirname, 'src/'),
