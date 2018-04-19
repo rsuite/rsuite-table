303d55d3a64f9a4d935f85b9638780286eabd884
simonguo <simonguo.2009@gmail.com>
2018-04-19 16:13:43 +0800

    Fixes for eslint

--- karma.conf.js
+++ karma.conf.js
@@ -3,32 +3,29 @@ const webpackConfig = {
     pathinfo: true
   },
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      use: [
-        'babel-loader'
-      ],
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      }
+    ]
   },
   devtool: 'eval'
 };
 
-module.exports = (config) => {
+module.exports = config => {
   const { env } = process;
   config.set({
     basePath: '',
     files: ['test/index.js'],
-    frameworks: [
-      'mocha',
-      'sinon-chai'
-    ],
+    frameworks: ['mocha', 'sinon-chai'],
     colors: true,
     reporters: ['mocha', 'coverage'],
 
     logLevel: config.LOG_INFO,
     preprocessors: {
-      'test/index.js': ['webpack'],
+      'test/index.js': ['webpack']
     },
     webpack: webpackConfig,
     webpackMiddleware: {
@@ -38,16 +35,15 @@ module.exports = (config) => {
     customLaunchers: {
       ChromeCi: {
         base: 'Chrome',
-        flags: ['--no-sandbox'],
-      },
+        flags: ['--no-sandbox']
+      }
     },
     coverageReporter: {
       dir: 'coverage',
       reporters: [
         { type: 'html' },
-        { type: 'lcov', subdir: 'lcov' }  // lcov
+        { type: 'lcov', subdir: 'lcov' } // lcov
       ]
     }
   });
 };
-
