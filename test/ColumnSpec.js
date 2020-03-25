e0ebf64187206874d0f6e5601dc2a4171fa53163
simonguo <simonguo.2009@gmail.com>
2020-03-25 15:19:08 +0800

    Update tests

--- test/ColumnSpec.js
+++ test/ColumnSpec.js
@@ -3,16 +3,11 @@ import { findDOMNode } from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
 import Column from '../src/Column';
 
-
 describe('Column', () => {
-
   it('Should output a null', () => {
-    const instance = ReactTestUtils.renderIntoDocument(
-      <Column />
-    );
+    const instance = ReactTestUtils.renderIntoDocument(<Column />);
 
     const instanceDom = findDOMNode(instance);
     assert.equal(instanceDom, null);
   });
-
 });
