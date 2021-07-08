c4a1b9eb435e4d7924a04a0015112189ecb3c407
simonguo <simonguo.2009@gmail.com>
2021-07-09 00:02:40 +0800

    bump 5.0.0-alpha.5

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-export { default as Table, TableProps, TableLocaleType } from './Table';
-export { default as Column, ColumnProps } from './Column';
-export { default as Cell, CellProps } from './Cell';
-export { default as HeaderCell, HeaderCellProps } from './HeaderCell';
-export { default as ColumnGroup, ColumnGroupProps } from './ColumnGroup';
+export { default as Table } from './Table';
+export { default as Column } from './Column';
+export { default as Cell } from './Cell';
+export { default as HeaderCell } from './HeaderCell';
+export { default as ColumnGroup } from './ColumnGroup';
+
+export type { TableProps, TableLocaleType } from './Table';
+export type { ColumnProps } from './Column';
+export type { CellProps } from './Cell';
+export type { HeaderCellProps } from './HeaderCell';
+export type { ColumnGroupProps } from './ColumnGroup';
 export type { StandardProps, SortType, RowDataType, RowKeyType } from './@types/common';
