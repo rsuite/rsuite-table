ef16bb19c7b66da9c96145caf231ecf58cc5ab6a
Doma <leishenghao@126.com>
2021-10-14 12:46:51 +0800

    feat(a11y): add aria-busy attribute to loading table

--- src/Table.tsx
+++ src/Table.tsx
@@ -938,6 +938,7 @@ const Table = React.forwardRef((props: TableProps, ref) => {
         // Its value is an integer equal to the total number of rows available, including header rows.
         aria-rowcount={data.length + 1}
         aria-colcount={colCounts.current}
+        aria-busy={loading}
         {...rest}
         className={classes}
         style={styles}
