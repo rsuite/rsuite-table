18d65673845daea6cd106d3121b2023de293d9b9
Simon Guo <simonguo.2009@gmail.com>
2022-04-21 16:03:04 +0800

    feat(HeaderCell): support renderSortIcon on `<HeaderCell>` (#341)

--- test/HeaderCellSpec.js
+++ test/HeaderCellSpec.js
@@ -5,26 +5,26 @@ import HeaderCell from '../src/HeaderCell';
 
 describe('HeaderCell', () => {
   it('Should output a header', () => {
-    const instanceDom = getDOMNode(<HeaderCell>test</HeaderCell>);
-    assert.equal(instanceDom.className, 'rs-cell-header');
+    const instance = getDOMNode(<HeaderCell>test</HeaderCell>);
+    assert.equal(instance.className, 'rs-cell-header');
   });
 
   it('Should output default sort icon', () => {
-    const instanceDom = getDOMNode(
+    const instance = getDOMNode(
       <HeaderCell sortable dataKey="name">
         test
       </HeaderCell>
     );
-    assert.isNotNull(instanceDom.querySelector('.rs-cell-header-icon-sort'));
+    assert.isNotNull(instance.querySelector('.rs-cell-header-icon-sort'));
   });
 
   it('Should output default sort desc icon', () => {
-    const instanceDom = getDOMNode(
+    const instance = getDOMNode(
       <HeaderCell sortable sortColumn="name" sortType="desc" dataKey="name">
         test
       </HeaderCell>
     );
-    assert.isNotNull(instanceDom.querySelector('.rs-cell-header-icon-sort-desc'));
+    assert.isNotNull(instance.querySelector('.rs-cell-header-icon-sort-desc'));
   });
 
   it('Should call `onSortColumn` callback', done => {
@@ -34,13 +34,13 @@ describe('HeaderCell', () => {
       }
     };
 
-    const instanceDom = getDOMNode(
+    const instance = getDOMNode(
       <HeaderCell width={100} onSortColumn={doneOp} sortable dataKey="name">
         test
       </HeaderCell>
     );
 
-    ReactTestUtils.Simulate.click(instanceDom.querySelector('.rs-cell'));
+    ReactTestUtils.Simulate.click(instance.querySelector('.rs-cell'));
   });
 
   it('Should call `onColumnResizeStart` callback', done => {
@@ -48,12 +48,64 @@ describe('HeaderCell', () => {
       done();
     };
 
-    const instanceDom = getDOMNode(
+    const instance = getDOMNode(
       <HeaderCell onColumnResizeStart={doneOp} resizable>
         test
       </HeaderCell>
     );
 
-    ReactTestUtils.Simulate.mouseDown(instanceDom.querySelector('.rs-column-resize-spanner'));
+    ReactTestUtils.Simulate.mouseDown(instance.querySelector('.rs-column-resize-spanner'));
+  });
+
+  it('Should render custom sort icons', () => {
+    const renderSortIcon = sortType => {
+      if (sortType === 'asc') {
+        return 1;
+      } else if (sortType === 'desc') {
+        return 2;
+      }
+      return 3;
+    };
+
+    const instance1 = getDOMNode(
+      <HeaderCell sortable dataKey="name" renderSortIcon={renderSortIcon}>
+        test
+      </HeaderCell>
+    );
+
+    assert.equal(instance1.querySelector('.rs-cell-header-sort-wrapper').textContent, 3);
+
+    const instance2 = getDOMNode(
+      <HeaderCell sortable dataKey="name" renderSortIcon={renderSortIcon} sortType="asc">
+        test
+      </HeaderCell>
+    );
+    assert.equal(instance2.querySelector('.rs-cell-header-sort-wrapper').textContent, 3);
+
+    const instance3 = getDOMNode(
+      <HeaderCell
+        sortable
+        dataKey="name"
+        renderSortIcon={renderSortIcon}
+        sortType="asc"
+        sortColumn="name"
+      >
+        test
+      </HeaderCell>
+    );
+    assert.equal(instance3.querySelector('.rs-cell-header-sort-wrapper').textContent, 1);
+
+    const instance4 = getDOMNode(
+      <HeaderCell
+        sortable
+        dataKey="name"
+        renderSortIcon={renderSortIcon}
+        sortType="desc"
+        sortColumn="name"
+      >
+        test
+      </HeaderCell>
+    );
+    assert.equal(instance4.querySelector('.rs-cell-header-sort-wrapper').textContent, 2);
   });
 });
