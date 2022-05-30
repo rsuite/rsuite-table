6fb5a69c15e609d170f7b6edcee555d1b4f7379c
Opinius <imaladec@gmail.com>
2022-05-30 13:04:17 +0600

    fix: rowHeight using is fixed in useTableDimension (#343)
    * rowHeight using is fixed in useTableDimension
    
    It is allowed in interface to pass rowHeight as function, but really it will not work correctly.
    Error is reproduced when Table is hidden.
    
    * lost dot

--- src/utils/useTableDimension.ts
+++ src/utils/useTableDimension.ts
@@ -73,13 +73,17 @@ const useTableDimension = (props: TableDimensionProps) => {
   const headerOffset = useRef<ElementOffset | null>(null);
   const tableOffset = useRef<ElementOffset | null>(null);
 
+  const getRowHeight = (rowData = {}) => {
+    return typeof rowHeight === 'function' ? rowHeight(rowData) : rowHeight;
+  };
+
   const calculateTableContextHeight = useCallback(() => {
     const prevContentHeight = contentHeight.current;
     const table = tableRef?.current;
     const rows = table?.querySelectorAll(`.${prefix?.('row')}`) || [];
 
     const nextContentHeight = rows.length
-      ? (Array.from(rows).map((row: Element) => getHeight(row) || rowHeight) as number[]).reduce(
+      ? (Array.from(rows).map((row: Element, index: number) => getHeight(row) || getRowHeight(data?.[index])) as number[]).reduce(
           (x: number, y: number) => x + y
         )
       : 0;
