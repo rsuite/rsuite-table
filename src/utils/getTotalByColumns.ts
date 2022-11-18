e5edba87176b2154934b92174e1d93baa1c4fcef
simonguo <simonguo.2009@gmail.com>
2022-11-18 18:27:52 +0800

    fix(Column): fix cannot customize columns

--- src/utils/getTotalByColumns.ts
+++ src/utils/getTotalByColumns.ts
@@ -1,15 +1,15 @@
 import React from 'react';
 import isPlainObject from 'lodash/isPlainObject';
-import { ColumnProps } from '../Column';
+import getColumnProps, { Column } from './getColumnProps';
 
-function getTotalByColumns(columns: React.ReactElement<ColumnProps> | React.ReactNode[]) {
+function getTotalByColumns(columns: Column | React.ReactNode[]) {
   let totalFlexGrow = 0;
   let totalWidth = 0;
 
   const count = (items: React.ReactNode[]) => {
     Array.from(items).forEach(column => {
       if (React.isValidElement(column)) {
-        const { flexGrow, width = 0 } = column.props as ColumnProps;
+        const { flexGrow, width = 0 } = getColumnProps(column as Column);
         totalFlexGrow += flexGrow || 0;
         totalWidth += flexGrow ? 0 : width;
       } else if (Array.isArray(column)) {
