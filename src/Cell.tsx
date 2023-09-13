b2287afd05098de2b53d6bc4246f92680e7aea92
Anatoly Pashin <anatoly.pashin@gmail.com>
2023-09-13 23:52:20 +0700

    feat(Cell): make Cell generic

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -69,154 +69,158 @@ const groupKeys = [
   'renderSortIcon'
 ];
 
-const Cell = React.forwardRef(<RowData = any>(props: InnerCellProps<RowData>, ref: React.Ref<HTMLDivElement>) => {
-  const {
-    classPrefix = 'cell',
-    width = 0,
-    left = 0,
-    headerHeight = ROW_HEADER_HEIGHT,
-    depth = 0,
-    height = ROW_HEIGHT,
-    style,
-    className,
-    fullText,
-    firstColumn,
-    lastColumn,
-    isHeaderCell,
-    align,
-    children,
-    rowData,
-    dataKey,
-    rowIndex,
-    removed,
-    rowKey,
-    rowSpan,
-    wordWrap,
-    verticalAlign,
-    expanded,
-    treeCol,
-    hasChildren,
-    predefinedStyle,
-    renderCell,
-    renderTreeToggle,
-    onClick,
-    onTreeToggle,
-    ...rest
-  } = props;
+const Cell = React.forwardRef(
+  <RowData = any,>(props: InnerCellProps<RowData>, ref: React.Ref<HTMLDivElement>) => {
+    const {
+      classPrefix = 'cell',
+      width = 0,
+      left = 0,
+      headerHeight = ROW_HEADER_HEIGHT,
+      depth = 0,
+      height = ROW_HEIGHT,
+      style,
+      className,
+      fullText,
+      firstColumn,
+      lastColumn,
+      isHeaderCell,
+      align,
+      children,
+      rowData,
+      dataKey,
+      rowIndex,
+      removed,
+      rowKey,
+      rowSpan,
+      wordWrap,
+      verticalAlign,
+      expanded,
+      treeCol,
+      hasChildren,
+      predefinedStyle,
+      renderCell,
+      renderTreeToggle,
+      onClick,
+      onTreeToggle,
+      ...rest
+    } = props;
 
-  const { rtl, hasCustomTreeCol, isTree } = React.useContext(TableContext);
+    const { rtl, hasCustomTreeCol, isTree } = React.useContext(TableContext);
 
-  const isTreeCol = treeCol || (!hasCustomTreeCol && firstColumn && isTree);
-  const cellHeight = typeof height === 'function' ? height(rowData!) : height;
+    const isTreeCol = treeCol || (!hasCustomTreeCol && firstColumn && isTree);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const cellHeight = typeof height === 'function' ? height(rowData!) : height;
 
-  if (isTreeCol && !isHeaderCell && !rowData) {
-    throw new Error('[Table.Cell]: `rowData` is required for tree column');
-  }
-
-  const handleTreeToggle = useCallback(
-    (event: React.MouseEvent) => {
-      onTreeToggle?.(rowKey, rowIndex, rowData, event);
-    },
-    [onTreeToggle, rowData, rowIndex, rowKey]
-  );
-
-  const { withClassPrefix, merge, prefix } = useClassNames(classPrefix);
-  const classes = merge(
-    className,
-    withClassPrefix({
-      expanded: expanded && isTreeCol,
-      first: firstColumn,
-      last: lastColumn,
-      rowspan: rowSpan && !isHeaderCell,
-      'full-text': fullText
-    })
-  );
-
-  const nextHeight = isHeaderCell ? headerHeight : cellHeight;
-  const styles = {
-    ...predefinedStyle,
-    [fullText ? 'minWidth' : 'width']: width,
-    height: nextHeight,
-    zIndex: depth,
-    [rtl ? 'right' : 'left']: left
-  };
-
-  const paddingKey = rtl ? 'paddingRight' : 'paddingLeft';
-  const contentStyles: React.CSSProperties = {
-    ...style,
-    width: fullText ? width - 1 : width,
-    height: nextHeight,
-    textAlign: align,
-    [paddingKey]: isTreeCol ? depth * LAYER_WIDTH + 10 : style?.[paddingKey] || style?.padding
-  };
-
-  if (verticalAlign) {
-    contentStyles.display = 'table-cell';
-    contentStyles.verticalAlign = verticalAlign;
-  }
-
-  if (wordWrap) {
-    contentStyles.wordBreak = typeof wordWrap === 'boolean' ? 'break-all' : wordWrap;
-    contentStyles.overflowWrap = wordWrap === 'break-word' ? wordWrap : undefined;
-  }
-
-  let cellContent = isNil(children) && rowData && dataKey ? get(rowData, dataKey) : children;
-
-  if (typeof children === 'function') {
-    cellContent = children(rowData!, rowIndex);
-  }
-
-  const renderTreeNodeExpandIcon = () => {
-    const ExpandIconComponent = expanded ? ArrowDown : ArrowRight;
-    const expandButton = <ExpandIconComponent className={prefix('expand-icon')} />;
-
-    if (isTreeCol && hasChildren) {
-      return (
-        <span
-          role="button"
-          tabIndex={-1}
-          className={prefix('expand-wrapper')}
-          onClick={handleTreeToggle}
-        >
-          {renderTreeToggle ? renderTreeToggle(expandButton, rowData, expanded) : expandButton}
-        </span>
-      );
+    if (isTreeCol && !isHeaderCell && !rowData) {
+      throw new Error('[Table.Cell]: `rowData` is required for tree column');
     }
 
-    return null;
-  };
+    const handleTreeToggle = useCallback(
+      (event: React.MouseEvent) => {
+        onTreeToggle?.(rowKey, rowIndex, rowData, event);
+      },
+      [onTreeToggle, rowData, rowIndex, rowKey]
+    );
 
-  const content = wordWrap ? (
-    <div className={prefix('wrap')}>
-      {renderTreeNodeExpandIcon()}
-      {renderCell ? renderCell(cellContent) : cellContent}
-    </div>
-  ) : (
-    <>
-      {renderTreeNodeExpandIcon()}
-      {renderCell ? renderCell(cellContent) : cellContent}
-    </>
-  );
+    const { withClassPrefix, merge, prefix } = useClassNames(classPrefix);
+    const classes = merge(
+      className,
+      withClassPrefix({
+        expanded: expanded && isTreeCol,
+        first: firstColumn,
+        last: lastColumn,
+        rowspan: rowSpan && !isHeaderCell,
+        'full-text': fullText
+      })
+    );
 
-  if (removed) {
-    return null;
-  }
+    const nextHeight = isHeaderCell ? headerHeight : cellHeight;
+    const styles = {
+      ...predefinedStyle,
+      [fullText ? 'minWidth' : 'width']: width,
+      height: nextHeight,
+      zIndex: depth,
+      [rtl ? 'right' : 'left']: left
+    };
 
-  return (
-    <div
-      ref={ref}
-      role={isHeaderCell ? 'columnheader' : 'gridcell'}
-      {...omit(rest, [...groupKeys, ...columnHandledProps])}
-      onClick={onClick}
-      className={classes}
-      style={styles}
-    >
-      <div className={prefix('content')} style={contentStyles}>
-        {content}
+    const paddingKey = rtl ? 'paddingRight' : 'paddingLeft';
+    const contentStyles: React.CSSProperties = {
+      ...style,
+      width: fullText ? width - 1 : width,
+      height: nextHeight,
+      textAlign: align,
+      [paddingKey]: isTreeCol ? depth * LAYER_WIDTH + 10 : style?.[paddingKey] || style?.padding
+    };
+
+    if (verticalAlign) {
+      contentStyles.display = 'table-cell';
+      contentStyles.verticalAlign = verticalAlign;
+    }
+
+    if (wordWrap) {
+      contentStyles.wordBreak = typeof wordWrap === 'boolean' ? 'break-all' : wordWrap;
+      contentStyles.overflowWrap = wordWrap === 'break-word' ? wordWrap : undefined;
+    }
+
+    let cellContent = isNil(children) && rowData && dataKey ? get(rowData, dataKey) : children;
+
+    if (typeof children === 'function') {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      cellContent = children(rowData!, rowIndex);
+    }
+
+    const renderTreeNodeExpandIcon = () => {
+      const ExpandIconComponent = expanded ? ArrowDown : ArrowRight;
+      const expandButton = <ExpandIconComponent className={prefix('expand-icon')} />;
+
+      if (isTreeCol && hasChildren) {
+        return (
+          <span
+            role="button"
+            tabIndex={-1}
+            className={prefix('expand-wrapper')}
+            onClick={handleTreeToggle}
+          >
+            {renderTreeToggle ? renderTreeToggle(expandButton, rowData, expanded) : expandButton}
+          </span>
+        );
+      }
+
+      return null;
+    };
+
+    const content = wordWrap ? (
+      <div className={prefix('wrap')}>
+        {renderTreeNodeExpandIcon()}
+        {renderCell ? renderCell(cellContent) : cellContent}
       </div>
-    </div>
-  );
-});
+    ) : (
+      <>
+        {renderTreeNodeExpandIcon()}
+        {renderCell ? renderCell(cellContent) : cellContent}
+      </>
+    );
+
+    if (removed) {
+      return null;
+    }
+
+    return (
+      <div
+        ref={ref}
+        role={isHeaderCell ? 'columnheader' : 'gridcell'}
+        {...omit(rest, [...groupKeys, ...columnHandledProps])}
+        onClick={onClick}
+        className={classes}
+        style={styles}
+      >
+        <div className={prefix('content')} style={contentStyles}>
+          {content}
+        </div>
+      </div>
+    );
+  }
+);
 
 Cell.displayName = 'Table.Cell';
 Cell.propTypes = {
