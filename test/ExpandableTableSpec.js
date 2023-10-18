7d0278c6f41b21d5c50d0110485bb87b532b05ad
Abdelrhman Gamal <abdo.gamy2010@gmail.com>
2023-10-18 12:49:48 +0300

    add feature unit test

--- test/ExpandableTableSpec.js
+++ test/ExpandableTableSpec.js
@@ -65,4 +65,38 @@ describe('ExpandableTable', () => {
       '46px'
     );
   });
+
+  it('Should the exapanable areas has different heights ', () => {
+    const data = [
+      { id: 1, name: 'foobar' },
+      { id: 2, name: 'foobar2' },
+      { id: 3, name: 'foobar3' }
+    ];
+
+    const { container } = render(
+      <Table
+        data={data}
+        rowKey="id"
+        showHeader={false}
+        expandedRowKeys={[1, 2, 3]}
+        rowExpandedHeight={rowData => {
+          return rowData.id * 100;
+        }}
+        renderRowExpanded={() => {
+          return <div>content</div>;
+        }}
+      >
+        <Column width={130}>
+          <HeaderCell>Name</HeaderCell>
+          <Cell dataKey="name" />
+        </Column>
+      </Table>
+    );
+    const rowExpandedHeights = [];
+    container
+      .querySelectorAll('.rs-table-row-expanded')
+      .forEach(el => rowExpandedHeights.push(el.offsetHeight));
+
+    expect(rowExpandedHeights).to.deep.equal([100, 200, 300]);
+  });
 });
