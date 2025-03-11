c9d31ccbc3e3aeae93a2e75649698ae745f88925
simonguo <simonguo.2009@gmail.com>
2025-03-11 14:12:52 +0800

    refactor: update table utils and dependencies

--- src/utils/children.ts
+++ src/utils/children.ts
@@ -1,12 +1,12 @@
 import React from 'react';
-import * as ReactIS from 'react-is';
+import { isFragment } from './react-is';
 
 export const flattenChildren = (
   children: React.ReactNode | React.ReactNode[],
   flattened: React.ReactNode[] = []
 ) => {
   for (const child of React.Children.toArray(children)) {
-    if (ReactIS.isFragment(child)) {
+    if (isFragment(child)) {
       const childEl = child as React.ReactElement;
       if (childEl.props?.children) {
         flattenChildren(childEl.props.children, flattened);
