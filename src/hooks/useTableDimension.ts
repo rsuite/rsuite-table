9818a6ecfd788d16e8839df3e68c991996b7fffd
Leon9916 <134840214+Leon9916@users.noreply.github.com>
2026-03-31 02:07:27 +0800

    fix(table): 使用 SCROLLBAR_WIDTH 常量替代硬编码的魔法数字 8
    在 useTableDimension 的 getTableHeight 方法中使用常量替代硬编码值

--- src/hooks/useTableDimension.ts
+++ src/hooks/useTableDimension.ts
@@ -343,7 +343,7 @@ const useTableDimension = <Row extends RowDataType, Key>(props: TableDimensionPr
 
     // fix: https://github.com/rsuite/rsuite/issues/557
     if(maxHeight && height < maxHeight && contentWidth.current > tableWidth.current) {
-      height += 8
+      height += SCROLLBAR_WIDTH!
     }
 
     return height;
